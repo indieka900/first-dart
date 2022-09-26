25415ce47c75cf514e8d4111802d16df9b2d8267
indieka900 <indiekaj@gmail.com>
2022-09-26 10:03:51 +0300

    first dart coding

--- tutorial.dart
+++ tutorial.dart
@@ -13,6 +13,29 @@ String prompt(var prompText) {
   return "${answer}";
 }
 
+  void gfg1(int g1, [ int? g2 ]) // for null values we use ? character
+{
+	// Creating function 1
+  // we use 'required' for the parameters that should not be left black
+	print("g1 is $g1");
+	print("g2 is $g2");
+}
+
+void gfg2(int g1,  { int? g2, int? g3 })
+{
+	// Creating function 2
+	print("g1 is $g1");
+	print("g2 is $g2");
+	print("g3 is $g3");
+}
+
+void gfg3(int g1, { int g2 : 12 })
+{
+	// Creating function 3
+	print("g1 is $g1");
+	print("g2 is $g2");
+}
+
 void main() {
   //data types
   var a = 50;
@@ -110,9 +133,21 @@ void main() {
     //print(l);
     l++;
     //print(l);
+
+  print("Calling the function with optional parameter:");
+	gfg1(01);
+
+	// Calling the function with Optional Named parameter
+	print("Calling the function with Optional Named parameter:");
+	gfg2(01, g3 : 12);
+
+	// Calling function with default valued parameter
+	print("Calling function with default valued parameter");
+	gfg3(01);
   }
 
   String mystr = 'Hello';
   String mystr1 = ' people';
-  print(mystr + mystr1);
+  String raws = r'This is my first raw string';
+  print(mystr + mystr1 + raws);
 }
