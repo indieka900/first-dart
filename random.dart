ae4705e227afea91bb5af104dbbffa43c469fdab
indieka900 <indiekaj@gmail.com>
2022-09-27 13:23:14 +0300

    first dart coding

--- random.dart
+++ random.dart
@@ -4,8 +4,8 @@ Random rndop = new Random();
 List<String> op = ['+', '*', '/', '-'];
 
 void main() {
-  double intValue = Random().nextInt(30) + 5; // Value is >= 0 and < 30.
-  double Value = Random().nextInt(100) + 50; // Value is >= 50 and < 150.
+  double intValue = Random().nextInt(40) + 5; // Value is >= 0 and < 30.
+  double Value = Random().nextInt(15) + 50; // Value is >= 50 and < 150.
   double intValue1 = Random().nextInt(8) - 3; // Value is >= 0 and < 30.
   double Value1 = Random().nextInt(10) + intValue;
   var tobe = op[rndop.nextInt(op.length)];
@@ -19,5 +19,10 @@ void main() {
     print("$Value1 $tobe $intValue =");
     tobe = op[rndop.nextInt(op.length)];
     //print(tobe);
+  }
+  if (Value <= 17 || intValue1 <= 17) {
+    if (Value1 >= 100 && intValue >= Value) {
+      print("Nested if archieved.");
+    }
   }
 }
