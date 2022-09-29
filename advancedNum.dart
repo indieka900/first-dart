9b62df38a6317fd5e96fe5dd49b04b73b59b7afa
indieka900 <indiekaj@gmail.com>
2022-09-29 11:01:01 +0300

    numbers in dart

--- advancedNum.dart
+++ advancedNum.dart
@@ -7,4 +7,14 @@ main() {
   print(myLongnum.toStringAsPrecision(5));
   print(myLongnum.ceilToDouble());
   print(myLongnum.roundToDouble()); // round up to a decimal number
+
+//checks if the string can be double
+  double? tr = double.tryParse('234uss7638329');
+  print(tr);//prints null because this variable cannot be double
+  double? tr1 = double.tryParse('23638329.901');
+  print(tr1);
+
+//converting int to double
+  int tr2 = 12435;
+  print(tr2.toString());
 }
