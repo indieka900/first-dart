ef030b8abf4fd532adfce68e14208abcb519c8d1
indieka900 <indiekaj@gmail.com>
2022-10-02 00:40:01 +0300

    parameters in dart

--- parameters.dart
+++ parameters.dart
@@ -1,1 +1,10 @@
-main(){}
+main() {
+  findVolume(4);//use the default values for the breath and lenght
+  findVolume(9, length: 17); //uses default value for breath  and overides that of length with the new one
+  findVolume(5, breath: 12, length: 30); //overides the default values
+}
+
+findVolume(int height, {int breath = 20, int length = 14}) {
+  double volume = (height * breath * length) / 3;
+  print("The volume is ${volume.round()} for the $height, $length and $breath \n");
+}
